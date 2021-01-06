92f531bc1e56cf1f24f8ecd0b74fe16aa175287c
unknown <drk3931@gmail.com>
2021-01-06 18:16:10 -0500

    web deploy updated

--- plat_plus/core/src/com/drk3931/platplus/Updateable.java
+++
@@ -1,8 +0,0 @@
-package com.drk3931.platplus;
-
-public interface Updateable{
-
-    public void update(float delta);
-
-
-}
--- plat_plus/core/src/com/drk3931/platplus/effects/Effect.java
+++
@@ -1,7 +0,0 @@
-package com.drk3931.platplus.effects;
-
-import com.drk3931.platplus.Entity;
-
-public interface Effect{
-    public void apply(Entity e);
-}
